a49d9aeb9d54bed1f2a5eb13ece8514f99f0b68b
Seok Jun Kang <swings134@gmail.com>
2022-03-23 19:10:51 +0900

    insert sort

--- src/array/RandomSort.java
+++ src/array/RandomSort.java
@@ -1,7 +1,6 @@
 package array;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -30,24 +29,26 @@ public class RandomSort {
 		
 		// -------------------- 정렬 ------------------------------
 		
-		sort(list);
-		System.out.println("정렬 후 : " + Arrays.toString(list)); // 난수 발생확인
+		sort(list);	 // method 호출.
+		System.out.println("정렬 후 : " + Arrays.toString(list)); // 정렬 확인
 		
 	}//main
 	
 	// 선택 정렬 (Insert Sort)
 	static void sort(int[] list) {
+				//기준값 = 앞, 비교값 = 기준값 + 1
+		
 				int prim = 0; //기준값 => 비교대상 
 				int j = 0;
 				
 				for (int i = 1; i < list.length; i++) {
-					prim = list[i];
+					prim = list[i]; // 임시저장할대상 = 기준값 + 1  
 					
-					for (j = i -1; j >= 0 && list[j] > prim; j--) {
-						list[j + 1] = list[j];
+					for (j = i -1; j >= 0 && list[j] > prim; j--) { //비교 : j = index 0, i = 1
+						list[j + 1] = list[j]; // 비교값 = 기준값, 비교대상이 큰경우 오른쪽으로 밀어버림.
 						
 					}
-					list[j+1] = prim;
+					list[j+1] = prim; // 기준값 저장.
 					
 				} //for
 	}//sort
