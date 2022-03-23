2b77fb0f99ec4e5dfc43be8080ca36c3de6acf53
Seok Jun Kang <swings134@gmail.com>
2022-03-23 21:35:09 +0900

    Quick Sort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 /**
  * 
  * <pre>
- * Quick Sort : 퀵정렬
+ * Quick Sort : 퀵정렬(분할정복 알고리즘)
+ * 
  * 1. 하나의 리스트를 Pivot 기준으로 두개의 부분리스트로 나눔.
  * 2. 피벗을 기준으로 양쪽에서 Pivot 보다 큰값, 혹은 작은값 검색
  * 3. 왼쪽에서는 Pivot 보다 큰값 검색
@@ -19,6 +20,11 @@ import java.util.Arrays;
 		  +---------------------------------------------------------+
 		  
 		  
+ *	point : strt 와 pivot 값 비교후 
+ *			pivot 보다 크면 정지. 후 swap 
+ *			-- 마찬가지로 end 가 pivot 보다 작으면 정지 후 swap 
+ *		  >> 해당 코드 do - while 문 
+ *
  * </pre>
  * @Class   : 
  * @File    : QuickSort.java
@@ -40,33 +46,38 @@ public class QuickSort {
 	 */
 	private static void QuickSortMethod(int[] a, int left, int right) {
 		
-		int lo = left;						// 왼쪽 커서
-		int hi = right;						// 오른쪽 커서
+		int strt = left;						// 왼쪽 커서
+		int end = right;						// 오른쪽 커서
 		int pivot = a[(left + right) / 2]; // 중간
 	
 			
 		do {
-			while(a[lo] < pivot) lo++;
-			while(a[hi] > pivot) hi--;
-			if(lo <= hi) {
-				swap(a, lo++, hi--);
-			}
-		} while(lo <= hi);
+			while(a[strt] < pivot) strt++;		// 왼쪽커서가 pivot 값 보다 작으면 커서++;		==> 
+			while(a[end] > pivot) end--;		// 오른쪽커서가 pivot 값 보다 크면 커서--; 오른쪽에서 줄어야함으로.
 			
-		if(left < hi) {
-			QuickSortMethod(a, left, hi);	// 재귀
-		}
-		if(lo < right ) {
-			QuickSortMethod(a, lo, right);	// 재귀
+			if(strt <= end) {
+				swap(a, strt++, end--);			
+			}
+		} while(strt <= end);					// 왼쪽커서와 오른쪽 커서가 같아지면 중지.
+		
+		
+		if(left < end) {						// 왼쪽 커서(입력값0)가 오른쪽 커서(end)보다 작으면 실행. 즉 1차 정렬후 왼쪽 부분리스트 재정렬.
+			QuickSortMethod(a, left, end);		// 재귀를 통한 왼쪽리스트내부 정렬.
 		}
 		
-	}
+		if(strt < right ) {						// 왼쪽 커서가 배열-1 보다 작으면 실행.
+			QuickSortMethod(a, strt, right);	// 재귀를 통한 오른쪽 리스트 정렬.
+		}
+		
+	}//QuickSortMethod
 	
-	// 두 수의 교환 메서드
+	// 두 수의 교환 메서드 : swap
 	private static void swap(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
+		
+		System.out.println(Arrays.toString(a));
 	}
 	
 	public static void main(String[] args) {
