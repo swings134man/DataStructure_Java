13000179d16a3acfbc7cb010ec356b01e60e4d54
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-01-22 19:13:05 +0900

    class deCompile && Lombok Lib(add) && LomBok class

---
+++ src/lombok/testDTO.java
@@ -0,0 +1,14 @@
+package lombok;
+
+@Data
+public class testDTO {
+
+//    @Getter(AccessLevel.NONE)
+//    @Setter(AccessLevel.NONE)
+    private boolean is_active;
+
+    private boolean test_active;
+
+    private String test;
+
+}
---
+++ src/lombok/testLombokMain.java
@@ -0,0 +1,14 @@
+package lombok;
+
+public class testLombokMain {
+
+
+    public static void main(String[] args) {
+        testDTO dto = new testDTO();
+//        dto.set_active(false);
+//        boolean active = dto.is_active();
+//        System.out.println(active);
+
+    }
+
+}
