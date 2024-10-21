b7362378e3ac2d76adb711617aa3cfb7ed175dc9
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-10-22 01:05:00 +0900

    docs: 알고리즘 복잡도(시간복잡도)

---
+++ src/test/TestCode3.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestCode3 {
+    public static void main(String[] args) {
+        int total = 0;
+        int n = 100;
+
+        for(int i=0; i<=n; i++) {
+          total += i;
+        }
+        System.out.println(total);
+    }
+}
